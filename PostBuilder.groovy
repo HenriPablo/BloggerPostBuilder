2bbab9412b1f801250cd439597c595a244589bc6
tomekpilot <tomekpilot@gmail.com>
2015-12-04 06:00:51 -0500

    refining the build output

--- PostBuilder.groovy
+++ PostBuilder.groovy
@@ -1,9 +1,8 @@
 #! /usr/bin/env groovy
 
-
 import sun.font.TrueTypeFont;
 
-println "\nWelcome to Groovy Blogger Post Builder"
+println "\nWelcome to Groovy Blogger Post Builder\n";
 
 // get path to directory we're in
 basePath = new File(".").getCanonicalPath();
@@ -26,7 +25,7 @@ scriptFile = getClass().protectionDomain.codeSource.location.path
 
 
 def title = "";
-def commonFileName = "default";
+def commonFileName = "";
 
 // use date as part of default post name
 date = new Date();
@@ -41,7 +40,6 @@ date = new Date();
 
 def defaultConfig  = new ConfigSlurper().parse( new File( scriptDir + '/default.config').toURI().toURL() );
 
-//println defaultConfig;
 
 
 /* START parsing command line args */
@@ -67,8 +65,8 @@ if( strArgs.size == 0 ){
 			// replace spaces with hyphens in title
 			if( p[0] == "title")
 			{
-				commonFileName = p[1].replaceAll( " ", "-")
-				p[1] = p[1].replaceAll( " ", "-")
+				commonFileName = p[1].toLowerCase().replaceAll( " ", "-");
+				//p[1] = p[1].replaceAll( " ", " ")
 				defaultConfig.config.title = p[1];
 			}
 		}
@@ -80,44 +78,53 @@ if( strArgs.size == 0 ){
 
 
 /* check for existence of custom config based on title of post */
-def customConfig 
-def customConfigFile = new File( basePath + "/" + defaultConfig.config.title + ".config");
+def customConfig;
+def customConfigFile = new File( basePath + "/" + commonFileName + ".config");
 
 if( customConfigFile.exists()){	
-	customConfig = new ConfigSlurper().parse( new File( defaultConfig.config.title + '.config' ).toURI().toURL() );
+	customConfig = new ConfigSlurper().parse( new File( commonFileName + '.config' ).toURI().toURL() );
 } else {
-	new File( defaultConfig.config.title + '.config').withWriter{
+	new File( commonFileName + '.config').withWriter{
 		writer ->
 				defaultConfig.writeTo(writer);
 	}
-	customConfig = new ConfigSlurper().parse( new File( defaultConfig.config.title + '.config' ).toURI().toURL() );
+	customConfig = new ConfigSlurper().parse( new File( commonFileName + '.config' ).toURI().toURL() );
 }
 
 /* DEFAULT CSS, JS file */
-def defaultCss = new File( scriptDir + "/" + commonFileName + ".css");
-def defaultJs = new File( scriptDir + "/" + commonFileName + ".js");
+def defaultCss = new File( scriptDir + "/default.css");
+def defaultJs = new File( scriptDir + "/default.js");
 
 
 if( !defaultCss.exists()){
-	new File( scriptDir + "/" + commonFileName + ".css").write("");
+	new File( scriptDir + "/default.css").write("");
 }
 
 if( !defaultJs.exists()){
-	new File( scriptDir + "/" + commonFileName + ".js").write("");
+	new File( scriptDir + "/default.js").write("");
 }
 
 
-/* CUSTOM - POST HTML CONTENTS */
-def customHtml = new File( basePath + "/" + customConfig.config.title + ".html");
+/* CUSTOM - HTML CONTENTS */
+def customHtml = new File( basePath + "/" + commonFileName + ".html");
 
+/* if it didn't exist before, read in the one just created */
 if( !customHtml.exists()){
-	// make this optional: new File( basePath + "/" + customConfig.config.title + ".html" ).write("<h1>" + customConfig.config.title + "</h1>");
-	
-	/* if it didn't exist before, read in the one just created */
-	customHtml = new File( basePath + "/" + customConfig.config.title + ".html");
+	customHtml = new File( basePath + "/" + commonFileName + ".html");
 }
 
 
+/* CUSTOM - CSS CONTENTS */
+def customCss = new File( basePath + "/" + commonFileName + ".css");
+if( !customCss.exists()){
+	 new File( basePath + "/" + commonFileName + ".css").write("");
+	 println "Custom CSS has ${customCss.length()} bytes";
+}
+/* CUSTOM - JS CONTENTS */
+def customJs = new File( basePath + "/" + commonFileName + ".js");
+if( !customJs.exists()){
+	 new File( basePath + "/" + commonFileName + ".js").write("");
+}
 /* 
  * THE HEART OF THE POST 
  * 	this holds DEV or PRODUCTION post contents of HTML, CSS and JS
@@ -126,13 +133,16 @@ String outputString = "";
 
 /* CSS */
 
-if( defaultCss.exists() ){
+if( defaultCss.exists() && !customCss ){
 	outputString += blockTxt( defaultCss, 'css', customConfig );
+} else if( customCss.exists() ){
+	outputString += blockTxt( customCss, 'css', customConfig );
+	println "${customCss.length()} bytes written to Custom CSS";
 }
 
 /* JAVASCRIPT */
 
-if( customConfig.config.dev == true ){ /* include jQuery from CDN */
+if( customConfig.config.dev == true || customConfig.config.extJS == true ){ /* include jQuery from CDN */
 	/*
 		TODO: move this into config or at least allow for version to be arg based,
 	use some default version is not provided
@@ -141,8 +151,10 @@ if( customConfig.config.dev == true ){ /* include jQuery from CDN */
     outputString += '\n';
 }
 
-if( defaultJs.exists() ){
-    outputString += blockTxt(defaultJs, 'js', customConfig )
+if( defaultJs.exists() && !customJs.exists() ){
+    outputString += blockTxt(defaultJs, 'js', customConfig );
+} else if( customJs.exists() ){
+	outputString += blockTxt(customJs, 'js', customConfig )
 }
 
 
@@ -154,18 +166,20 @@ if( customHtml.exists() ){
 /* do we need to create the whole file setup?*/
 if( customConfig.config.create == "all" ){
 	println "\n trying to call createFiles() \n";
-	println "title : " + customConfig.config.title + "\n";
-	this.createFiles( customConfig.config.title );
+	println "title : " + commonFileName + "\n";
+	this.createFiles( commonFileName );
 }
 
 
-new File( basePath + "/" + customConfig.config.title + "-composit.html").write( outputString.trim() );
+new File( basePath + "/" + commonFileName + "-composit.html").write( outputString.trim() );
 
 // open composit file in default browser
 // credit goes to: http://www.centerkey.com/java/browser/
 if( customConfig.config.preview == true){
-	String url = "file://${basePath}/${ customConfig.config.title }-composit.html";
-	java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+	String url = "file://${basePath}/${ commonFileName }-composit.html";
+	url = new URL(url).text;
+	//java.awt.Desktop.getDesktop().browse(java.net.URI.create( url ));
+	java.awt.Desktop.desktop.browse url.toURI()
 }
 
 /**
@@ -184,11 +198,11 @@ def blockTxt( File file, String fileType, customConfig ){
 		
 		case 'css' :
 			if( dev ){
-				result += "<html><head><title>" +  customConfig.config.title + "</title>"
+				result += "<html><head><title>" +  customConfig.config.title + "</title>\n"
 			}
 			result += '<style type="text/css">\n'
 			result += file.getText();
-			result += '\n</style>'
+			result += '\n</style>\n'
 			break
 		
         case 'js' :
@@ -244,4 +258,3 @@ def showHelp(){
 	println "See 'blogger-post-builder-read-me.txt' for more information.";
 	System.exit(0);
 }
-
